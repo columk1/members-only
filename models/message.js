109503733725a3883e52213578623f12c9b4be66
Colum Kelly <columk1@users.noreply.github.com>
2023-12-09 10:52:02 -0800

    Update populatedb with more data

--- models/message.js
+++ models/message.js
@@ -5,9 +5,9 @@ const Schema = mongoose.Schema
 
 const MessageSchema = new Schema(
   {
-    title: { type: String, required: [true, 'Title is required'], unique: true, maxLength: 100 },
+    title: { type: String, required: [true, 'Title is required'], unique: true, maxLength: 180 },
     // timestamp: { type: Date },
-    text: { type: String, required: [true, 'Message text is required'], maxLength: 100 },
+    text: { type: String, required: [true, 'Message text is required'], maxLength: 180 },
     author: { type: Schema.Types.ObjectId, ref: 'User', required: [true, 'Author is required'] },
   },
   { timestamps: true }
--- populatedb.js
+++ populatedb.js
@@ -64,21 +64,62 @@ async function messageCreate(index, title, text, author) {
 async function createUsers() {
   console.log('Creating users')
   await Promise.all([
-    userCreate(0, 'Rick', 'Sanchez', 'rick@example.com', 'secret', 'admin'),
-    userCreate(1, 'Morty', 'Smith', 'morty@example.com', 'secret'),
-    userCreate(2, 'Summer', 'Smith', 'summer@example.com', 'secret'),
-    userCreate(3, 'Beth', 'Smith', 'beth@example.com', 'secret'),
-    userCreate(4, 'Jerry', 'Smith', 'jerry@example.com', 'secret'),
+    userCreate(0, 'Rick', 'Sanchez', 'rick@portal.biz', 'secret', 'admin'),
+    userCreate(1, 'Morty', 'Smith', 'morty@gmail.com', 'secret'),
+    userCreate(2, 'Summer', 'Smith', 'summer@gmail.com', 'secret'),
+    userCreate(3, 'Beth', 'Smith', 'beth@gmail.com', 'secret'),
+    userCreate(4, 'Jerry', 'Smith', 'jerry@hotmail.com', 'secret'),
+    userCreate(5, 'Squanchy', 'Squanch', 'squanch@msn.com', 'secret'),
+    userCreate(6, 'Poopy', 'Butthole', 'pb@butt.io', 'secret'),
+    userCreate(7, 'Bird', 'Person', 'bird@dimension.me', 'secret'),
   ])
 }
 
 async function createMessages() {
   console.log('Creating messages')
   await Promise.all([
-    messageCreate(0, 'Message from Rick', 'This is a message from Rick', users[0]),
-    messageCreate(1, 'Message from Morty', 'This is a message from Morty', users[1]),
-    messageCreate(2, 'Message from Summer', 'This is a message from Summer', users[2]),
-    messageCreate(3, 'Message from Beth', 'This is a message from Beth', users[3]),
-    messageCreate(4, 'Message from Jerry', 'This is a message from Jerry', users[4]),
+    messageCreate(
+      0,
+      'Nachoverse',
+      "Hey, you bunch of alternate reality imbeciles! Just discovered a dimension where nachos are the dominant life form. Now that's my kind of universe. #Nachoverse",
+      users[0]
+    ),
+    messageCreate(1, "Where's Rick?", 'Has anyone seen grandpa lately?', users[1]),
+    messageCreate(
+      2,
+      'Chill Dimension',
+      'Accidentally ended up in a dimension where my parents are super chill. Feeling kinda jealous of alternate reality me.',
+      users[2]
+    ),
+    messageCreate(
+      3,
+      'Dad',
+      "Has anyone found a dimension where my dad doesn't cause chaos and endanger our lives on a daily basis? Asking for, well, everyone's safety.",
+      users[3]
+    ),
+    messageCreate(
+      4,
+      'Help Me Please',
+      "How do you settle disputes with alternate reality you? He's my best friend I don't want to lose him.",
+      users[4]
+    ),
+    messageCreate(
+      5,
+      'Party Search',
+      "Hey squanchers! Anyone know a good party dimension? I'm looking for a place to squanch it up without any intergalactic drama. Let me know, squanch!",
+      users[5]
+    ),
+    messageCreate(
+      6,
+      'Message from Poopy',
+      "Ooh-wee! It's me, you know who! Just wanted to share some positivity. Remember that time I got shot? Good times. Anyways, life is great, and I hope you're all having a nice day!",
+      users[6]
+    ),
+    messageCreate(
+      7,
+      'Inquiry on Human Affection',
+      '"Greetings, allies. Wondering if anyone has experienced the unmitigated contradictions of human affection.',
+      users[7]
+    ),
   ])
 }
